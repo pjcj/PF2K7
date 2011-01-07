488c90650274ceb4983004767bfdbd5b8778dd10
Paul Johnson <paul@pjcj.net>
2011-01-07 14:05:08 +0100

    Refactor common code in /register and /status.

--- lib/PF2K7/Controller/People.pm
+++ lib/PF2K7/Controller/People.pm
@@ -48,59 +48,78 @@ sub logout :Local :Args(0)
     $c->response->redirect($c->uri_for('/'));
 }
 
+{
+
+my @enneagrams1 = qw( unknown reformer helper motivator artist thinker
+                      loyalist enthusiast boss meditator );
+my @enneagrams2 = ( "none", @enneagrams1 );
+
+sub set_stash
+{
+    my ($self, $c) = @_;
+    $c->stash(enneagrams1 => \@enneagrams1);
+    $c->stash(enneagrams2 => \@enneagrams2);
+}
+
+sub validate_fields
+{
+    my ($self, $c, $p, $errors, $fields, @required) = @_;
+
+    $errors->{email} =
+        "Invalid email address (failed $Email::Valid::Details check)"
+        unless (Email::Valid->address(-address  => $p->{email},
+                                      -tldcheck => 1,
+                                      -mxcheck  => 0));
+
+    $errors->{enneagram1} = "Invalid enneagram"
+        unless $p->{enneagram1} ~~ @enneagrams1;
+    $errors->{enneagram2} = "Invalid enneagram"
+        unless $p->{enneagram2} ~~ @enneagrams2;
+
+    $errors->{gps} = "GPS coordinates invalid"
+        if length $p->{gps} && $p->{gps} !~
+            /^\s*\d+(?:\.\d+)? ?[NnSs] ?,? ?\d+(?:.\d+)? ?[EeWw]\s*$/;
+
+    for (@required)
+    {
+        $errors->{$_} = "Required field" unless length $p->{$_}
+    }
+
+    if (%$errors)
+    {
+        $c->stash(message => "Errors found on form");
+        $c->stash(errors  => $errors);
+        my %values; $values{$_} = $p->{$_} for @$fields;
+        $c->stash(values  => \%values);
+    }
+
+    !%$errors
+}
+
+}
+
 sub register :Local :Args(0)
 {
     my ($self, $c) = @_;
 
-    my @enneagrams1 = qw( unknown reformer helper motivator artist thinker
-                          loyalist enthusiast boss meditator );
-    my @enneagrams2 = ( "none", @enneagrams1 );
-    $c->stash(enneagrams1 => \@enneagrams1);
-    $c->stash(enneagrams2 => \@enneagrams2);
+    $self->set_stash($c);
 
     if (lc $c->req->method eq "post")
     {
         my $p = $c->req->params;
-        my %errors;
+        my $errors;
         my @fields = qw( username password name email town country motto1
                          motto2 likes dislikes gps enneagram1 enneagram2 );
 
         my $users_rs = $c->model("PF2K7::User");
-
-        $errors{username} = "Username already in use - please pick another"
+        $errors->{username} = "Username already in use - please pick another"
             if $users_rs->find({username => $p->{username}});
-
-        $errors{email} =
-            "Invalid email address (failed $Email::Valid::Details check)"
-            unless (Email::Valid->address(-address  => $p->{email},
-                                          -tldcheck => 1,
-                                          -mxcheck  => 0));
-
-        $errors{enneagram1} = "Invalid enneagram"
-            unless $p->{enneagram1} ~~ @enneagrams1;
-        $errors{enneagram2} = "Invalid enneagram"
-            unless $p->{enneagram2} ~~ @enneagrams2;
-
-        $errors{gps} = "GPS coordinates invalid"
-            if length $p->{gps} && $p->{gps} !~
-                /^\s*\d+(?:\.\d+)? ?[NnSs] ?,? ?\d+(?:.\d+)? ?[EeWw]\s*$/;
-
-        for (qw( username password name email town country motto1 motto2 ))
-        {
-            $errors{$_} = "Required field" unless length $p->{$_}
-        }
-
-        if (%errors)
-        {
-            $c->stash(message => "Errors found on form");
-            $c->stash(errors  => \%errors);
-            my %values; $values{$_} = $p->{$_} for @fields;
-            $c->stash(values  => \%values);
-            return;
-        }
+        return unless
+            $self->validate_fields($c, $p, \@fields,
+                                   qw( username password name email town
+                                       country motto1 motto2 ));
 
         my $newuser = $users_rs->create({ map { $_ => $p->{$_} } @fields });
-
         unless ($c->authenticate({ username => $p->{username},
                                    password => $p->{password} }))
         {
@@ -119,11 +138,7 @@ sub status :Local :Args(0)
 
     $c->go("/pf/home") unless $c->user_exists;
 
-    my @enneagrams1 = qw( unknown reformer helper motivator artist thinker
-                          loyalist enthusiast boss meditator );
-    my @enneagrams2 = ( "none", @enneagrams1 );
-    $c->stash(enneagrams1 => \@enneagrams1);
-    $c->stash(enneagrams2 => \@enneagrams2);
+    $self->set_stash($c);
 
     my @fields = qw( name email town country motto1
                      motto2 likes dislikes gps enneagram1 enneagram2 );
@@ -135,38 +150,11 @@ sub status :Local :Args(0)
     if (lc $c->req->method eq "post")
     {
         my $p = $c->req->params;
-        my %errors;
+        my $errors;
         my $users_rs = $c->model("PF2K7::User");
-
-        $errors{email} =
-            "Invalid email address (failed $Email::Valid::Details check)"
-            unless (Email::Valid->address(-address  => $p->{email},
-                                          -tldcheck => 1,
-                                          -mxcheck  => 0));
-
-        $errors{enneagram1} = "Invalid enneagram"
-            unless $p->{enneagram1} ~~ @enneagrams1;
-        $errors{enneagram2} = "Invalid enneagram"
-            unless $p->{enneagram2} ~~ @enneagrams2;
-
-        $errors{gps} = "GPS coordinates invalid"
-            if length $p->{gps} && $p->{gps} !~
-                /^\s*\d+(?:\.\d+)? ?[NnSs] ?,? ?\d+(?:.\d+)? ?[EeWw]\s*$/;
-
-        for (qw( name email town country motto1 motto2 ))
-        {
-            $errors{$_} = "Required field" unless length $p->{$_}
-        }
-
-        if (%errors)
-        {
-            # use Data::Dumper; $c->error(Dumper(\%errors));
-            $c->stash(message => "Errors found on form");
-            $c->stash(errors  => \%errors);
-            my %values; $values{$_} = $p->{$_} for @fields;
-            $c->stash(values  => \%values);
-            return;
-        }
+        return unless
+            $self->validate_fields($c, $p, \@fields,
+                                   qw( name email town country motto1 motto2 ));
 
         # push @fields, "password" if length $p->{password};
         my $user = $c->user->obj;
